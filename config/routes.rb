dbc830be0ebcc5415dd43b7bde789492107d9c65
Quborg <miloud.laftah@gmail.com>
2015-06-14 19:10:28 +0000

    add version: scope :v1

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
 	root 'application#hello'
 
 	constraints subdomain: 'api' do
-		resources :products
-		resources :users
+		namespace :api, path: '/', defaults: { format: :json } do
+			scope module: :v1 do
+				resources :products
+				resources :users
+			end
+		end
 	end
 
 end
