2c87a6a455c16eed81ffe3c2ea398a0789f03d73
Quborg <miloud.laftah@gmail.com>
2015-06-20 17:06:56 +0000

    add user 1t spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,11 @@
 FactoryGirl.define do
-  factory :user do
-    
-  end
+
+	factory :user do
+
+		email { FFaker::Internet.email }
+		password "12345678"
+		password_confirmation "12345678"
+
+	end
 
 end
