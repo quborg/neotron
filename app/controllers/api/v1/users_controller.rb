2ff1394d77c1805e8acb412b305e277e331b3c92
Quborg <miloud.laftah@gmail.com>
2015-06-21 00:44:56 +0000

    Adds show action the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,40 +1,9 @@
 class Api::V1::UsersController < ApplicationController
 
-
-
-	def index
-		users = Users.all
-		render json: users, status 200
-	end
+	respond_to :json
 
 	def show
-		user = User.find(params[:id])
-		render json: user, status 200
+		respond_with User.find(params[:id])
 	end
-
-	def create
-		user = User.new(user_params)
-		if user.save
-			render json: user, status: 201, location: user
-		else
-			render json: user.errors, status: 422
-		end
-	end
-
-	def update
-		user = User.find(params[:id])
-		if user.update(user_params)
-			render json: user, status: 200
-		else
-			render json: user.errors, status: 422
-		end
-	end
-
-	private
-
-		def user_params
-			params.require(:user).permit(:email, :password)
-		end
-
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,16 +2,6 @@ require 'spec_helper'
 
 describe User do
 
-	# before { @user = FactoryGirl.build(:user) }
-
-	# subject { @user }
-
-	# it { should respond_to(:email) }
-	# it { should respond_to(:password) }
-	# it { should respond_to(:password_confirmation) }
-
-	# it { should be_valid }
-
 	it { should validate_presence_of(:email) }
 	it { should validate_uniqueness_of(:email) }
 	it { should validate_confirmation_of(:password) }
