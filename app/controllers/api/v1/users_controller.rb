cce60869353aa04bb577e855135a2b8c62d92dff
Quborg <miloud.laftah@gmail.com>
2015-07-27 20:25:34 +0100

    Adds authorization for the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,10 @@
 class Api::V1::UsersController < ApplicationController
-
+	before_action :authenticate_with_token!, only: [:update, :destroy]
 	respond_to :json
 
 	def show
 		respond_with User.find(params[:id])
 	end
-
 	def create
 		user = User.new(user_params)
 		if user.save
@@ -14,24 +13,20 @@ class Api::V1::UsersController < ApplicationController
 			render json: { errors: user.errors }, status: 422
 		end
 	end
-
 	def update
-		user = User.find(params[:id])
+		user = current_user
 		if user.update(user_params)
 			render json: user, status: 200, location: [:api, user]
 		else
 			render json: { errors: user.errors }, status: 422
 		end
 	end
-
 	def destroy
-		user = User.find(params[:id])
-		user.destroy
+		current_user.destroy
 		head 204
 	end
 
 	private
-
 		def user_params
 			params.require(:user).permit(:email, :password, :password_confirmation)
 		end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -46,11 +46,13 @@ describe Api::V1::UsersController do
 	end
 
 	describe "PUT/PATCH #update" do
+		before(:each) do
+			@user = FactoryGirl.create :user
+			request.headers['Authorization'] =  @user.auth_token
+		end
 		context "when is successfully updated" do
 			before(:each) do
-				@user = FactoryGirl.create :user
-				patch :update, { 	id: @user.id,
-									user: { email: "newmail@example.com" } }, format: :json
+				patch :update, { id: @user.id, user: { email: "newmail@example.com" } }, format: :json
 			end
 			it "renders the json representation for the updated user" do
 				user_response = json_response
@@ -60,9 +62,7 @@ describe Api::V1::UsersController do
 		end
 		context "when is not created" do
 			before(:each) do
-				@user = FactoryGirl.create :user
-				patch :update, { 	id: @user.id,
-									user: { email: "bademail.com" } }, format: :json
+				patch :update, { id: @user.id, user: { email: "bademail.com" } }, format: :json
 			end
 			it "renders an errors json" do
 				user_response = json_response
@@ -79,6 +79,7 @@ describe Api::V1::UsersController do
 	describe "DELETE #destroy" do
 		before(:each) do
 			@user = FactoryGirl.create :user
+			api_authorization_header @user.auth_token
 			delete :destroy, { id: @user.id }, format: :json
 		end
 		it { should respond_with 204 }
